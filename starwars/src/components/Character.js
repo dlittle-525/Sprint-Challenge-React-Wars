9907117d7f773d9d68d0adf843982a875a1d0e4c
Douglas Little <douglas.a.little@gmail.com>
2020-04-18 00:17:27 -0400

    MVP

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,11 +1,21 @@
 // Write your Character component here
 import React from "react";
+import { Card, CardBody, CardImg, CardTitle, CardText, Col } from "reactstrap";
 
-const Character = () => {
+const Character = ({ character}) => {
 return (
-    <div>
-        <h1>Character</h1>
-    </div>
-)};
+        <Col xs="4" md="3" xl="2">
+            <Card>
+               <CardBody>
+                   <CardTitle>{character.name}</CardTitle>
+                   <CardImg>{Character.image}</CardImg>
+                   <CardText>{character.status}</CardText>
+                   <CardText>{character.species}</CardText>
+                   <CardText>{character.gender}</CardText>
+               </CardBody>
+            </Card>
+        </Col>
+    );
+};
 
 export default Character;
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,11 +1,31 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Character from "./Character"
+import { Container, Row } from "reactstrap";
 
-const CharacterList = () =>{
+export default function CharacterList() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://rickandmortyapi.com/api/character/")
+            .then(response => {
+                setCharacters(response.data.results);
+                console.log(response.data.results);
+            })
+            .catch(error => {
+                console.log("The data was not returned", error);
+            });
+    }, []);
+console.log("Characters", characters);
+
 return (
-    <div>
-        <Character />
-    </div>
+    <Container>
+        <Row>
+        {characters && characters.map(character => {
+            return <Character character={character} />;
+        })}
+        </Row>
+    </Container>
+
 )};
 
-export default CharacterList;
